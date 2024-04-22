db3d221c76d523c6b18d8f89880dcb4e3f114bc6
chethana613 <chetha@pdx.edu>
2024-04-21 18:55:58 -0700

    Publisher and Subscriber

---
+++ MS1-Gather and Transport/publisher.py
@@ -0,0 +1,25 @@
+from google.cloud import pubsub_v1
+import json
+
+project_id = "trimet-421005"
+topic_id = "my-topic"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+file_path = input("Enter file path: ")
+
+with open(file_path, "r") as f:
+    file_contents = f.read()
+    #print("File contents:", file_contents)
+    data = json.loads(file_contents)
+
+vehicle_ids = [4505,3257,3232,4033,4040,3940,3313,3609,3738,3960,3625,3126,4046,4207,3263,4513,3253,3567,3407,4017,3132,4032,3107,3101,4002,3265,3266,3943,2912,3526,4502,4016,2920,3707,3926,4205,3616,3139,3731,3753,3537,3717,3038,3148,3228,2918,3516,3150,3614,3213,3417,2935,3644,2936,3649,3160,3144,3569,3034,4230,4211,2919,2931,4056,4238,3028,3202,3019,2932,3937,3939,3715,4047,3022,3328,4019,3219,3532,2908,3751,3114,3217,3231,3119,3203,3327,3008,3023,3230,3727,3906,4227,3748,2939,3225,3131,3421,3021,3802,3643]  # Ragnarok Vehicle ids
+#vehicle_ids = [4505]
+for vehicle_id in vehicle_ids:
+    vehicle_data = data.get(str(vehicle_id), [])
+    #print(vehicle_data)
+    for record in vehicle_data:
+        data_str = json.dumps(record)
+        data_bytes = data_str.encode("utf-8")
+        future = publisher.publish(topic_path, data_bytes)
+        print(f"Published message for vehicle {vehicle_id} to {topic_path}.")
---
+++ MS1-Gather and Transport/subscriber.py
@@ -0,0 +1,49 @@
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+from datetime import datetime
+import sys
+import os
+
+# TODO(developer)
+project_id = "trimet-421005"
+subscription_id = "my-sub"
+# Number of seconds the subscriber should listen for messages
+timeout = 300.0
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_id}`
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f"Received {message}.")
+    message.ack()
+
+#file_name = input("Enter the file name to write the result to: ")
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+folder_name = "TriMetSubscribedRecords"
+file_name = f"Result_{current_datetime}.json"
+filepath = os.path.join(folder_name, file_name)
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+result = None
+
+with open(filepath, "w") as file:
+    original_stdout = sys.stdout
+    sys.stdout = file
+    with subscriber:
+        try:
+            # When `timeout` is not set, result() will block indefinitely,
+            # unless an exception is encountered first.
+            # Wait for messages
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel()
+            streaming_pull_future.result()
+        finally:
+            file.close()
+            sys.stdout = original_stdout
