5e80059745f91f0fc61e7b5734936e613391bf63
vaishnavisrinath <54988264+vaishnavisrinath@users.noreply.github.com>
2024-05-26 19:04:53 -0700

    Updated sub3.py

--- MILESTONE 3/sub3.py
+++ MILESTONE 3/sub3.py
@@ -1,112 +1,53 @@
 import json
 import os
-from concurrent.futures import TimeoutError
-from google.cloud import pubsub_v1
-from datetime import datetime, timedelta
-from psycopg2.extras import execute_batch
 import csv
-import tempfile
-import pandas as pd
+from google.cloud import pubsub_v1
+from datetime import datetime
 import psycopg2
 
-project_id = "trimet-421005" 
-subscription_id = "busdata2-sub"  
+# TODO: Replace with your project details
+project_id = "trimet-421005"
+subscription_id = "stop-topic-sub"
 subscription_path = pubsub_v1.SubscriberClient().subscription_path(project_id, subscription_id)
 
 # Database connection details
 db_params = {
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': 'helloworld',
-    'host': 'localhost',
-    'port': '5432'
+       'dbname':'postgres',
+       'user':'postgres',
+       'password':'helloworld',
+       'host':'localhost',
+       'port':'5432'
 }
 
-# Initialize received_messages counter
+# File to store processed combinations for uniqueness check
 received_messages = 0
-
+processed_combinations = set()
+first_service_key = None
+last_pattern_distance = {}
 today = datetime.now().strftime("%Y-%m-%d")
 file_name = f"received_eventsassert_copytable_{today}.json"
+# Temporary file path and header for CSV
 tempfile_path = 'tempassertion_data.csv'
-header = ['vehicle_number', 'leave_time', 'train', 'route_number', 'direction', 'service_key', 'trip_number',
-          'stop_time', 'arrive_time', 'dwell', 'location_id', 'door', 'lift', 'ons', 'offs', 'estimated_load',
-          'maximum_speed', 'train_mileage', 'pattern_distance', 'location_distance', 'x_coordinate', 'y_coordinate',
-          'data_source', 'schedule_status', 'trip_id']
+header = ['vehicle_number', 'leave_time', 'train', 'route_number', 'direction', 'service_key', 'trip_number', 'stop_time', 'arrive_time', 'dwell', 'location_id', 'door', 'lift', 'ons', 'offs', 'estimated_load', 'maximum_speed', 'train_mileage', 'pattern_distance', 'location_distance', 'x_coordinate', 'y_coordinate', 'data_source', 'schedule_status','trip_id']
 
-
-def create_stop_event_table(cur):
+def copy_csv_to_db(file_path, table_name, db_params):
     try:
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS StopEvent (
-                vehicle_number INT,
-                leave_time INT,
-                train INT,
-                route_number INT,
-                direction VARCHAR(10),
-                service_key VARCHAR(10),
-                trip_number INT,
-                stop_time INT,
-                arrive_time INT,
-                dwell INT,
-                location_id INT,
-                door INT,
-                lift INT,
-                ons INT,
-                offs INT,
-                estimated_load INT,
-                maximum_speed INT,
-                train_mileage FLOAT,
-                pattern_distance FLOAT,
-                location_distance FLOAT,
-                x_coordinate FLOAT,
-                y_coordinate FLOAT,
-                data_source INT,
-                schedule_status INT,
-                vehicle_id INT,
-                trip_id INT
-            );
-        """)
-        conn.commit()  # Commit after creating the table
-        print("Table StopEvent created successfully.")
-    except Exception as e:
-        print(f"Error creating table: {e}")
-
-
-def copy_csv_to_db(file_path, table_name, cur):
-    try:
-        print("Inserting data into table...")
+        conn = psycopg2.connect(**db_params)
+        cur = conn.cursor()
         with open(file_path, 'r') as f:
             next(f)  # Skip the header row
-            rows = [row for row in csv.reader(f)]
-            
-        # Construct the SQL INSERT statement
-        insert_statement = f"""
-            INSERT INTO {table_name} (
-                vehicle_number, leave_time, train, route_number, direction, service_key, trip_number,
-                stop_time, arrive_time, dwell, location_id, door, lift, ons, offs, estimated_load,
-                maximum_speed, train_mileage, pattern_distance, location_distance, x_coordinate,
-                y_coordinate, data_source, schedule_status, trip_id
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                %s, %s, %s, %s
-            )
-        """
-        
-        # Execute the INSERT statement using execute_batch
-        execute_batch(cur, insert_statement, rows)
-        
-        conn.commit()  # Commit after inserting all rows
-        print(f"Data inserted into {table_name} successfully.")
+            cur.copy_from(f, table_name, sep=',', null='')
+        conn.commit()
+        cur.close()
+        conn.close()
     except Exception as e:
         print(f"Error copying data to database: {e}")
-
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
     global received_messages
     received_messages += 1
     json_message = json.loads(message.data.decode('utf-8'))
-
-    # Extract data from JSON message
+    
     vehicle_number = json_message.get('vehicle_number', None)
     leave_time = json_message.get('leave_time', None)
     train = json_message.get('train', None)
@@ -174,6 +115,7 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         #message.ack()
         return
     
+    last_pattern_distance[trip_number] = pattern_distance
     
     # Assertion9: Arrival time must be less than or equal to the leave time
     if arrive_time is not None and leave_time is not None and arrive_time > leave_time:
@@ -187,57 +129,39 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         #message.ack()
         return
 
+    # Map the service key value
+    service_key_map = {'W': 'Weekday', 'U': 'Sunday', 'S': 'Saturday'}
+    if service_key in service_key_map:
+        service_key = service_key_map[service_key]
+
+    # Map the direction value
+    direction_map = {'0': 'Out', '1': 'Back'}
+    if direction in direction_map:
+        direction = direction_map[direction]
+
     # Data transformation
     with open(tempfile_path, mode='a', newline='') as file:
         writer = csv.writer(file)
-        # Write header if the file is empty
-        if os.stat(tempfile_path).st_size == 0:
-            writer.writerow(header)
-        writer.writerow([vehicle_number, leave_time, train, route_number, direction, service_key, trip_number,
-                         stop_time, arrive_time, dwell, location_id, door, lift, ons, offs, estimated_load,
-                         maximum_speed, train_mileage, pattern_distance, location_distance, x_coordinate,
-                         y_coordinate, data_source, schedule_status, trip_id])
-
+        writer.writerow([vehicle_number, leave_time, train, route_number, direction, service_key, trip_number, stop_time, arrive_time, dwell, location_id, door, lift, ons, offs, estimated_load, maximum_speed, train_mileage, pattern_distance, location_distance, x_coordinate, y_coordinate, data_source, schedule_status, trip_id])
+    
     with open(file_name, 'a') as file:
         file.write(json.dumps(json_message) + '\n')
-
-    print(f"Received {received_messages} messages.")
+    
+    # Copy the CSV data to the PostgreSQL table
+    copy_csv_to_db(tempfile_path, 'stop_events', db_params)
+    print("inserted {received_messages} data to db")
     message.ack()
 
 print(f"Listening for messages on {subscription_path}..\n")
 
-# Opening the connection
-conn = psycopg2.connect(**db_params)
-cur = conn.cursor()
+subscriber = pubsub_v1.SubscriberClient()
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
 
-if __name__ == "__main__":
+# Keep the subscriber listening
+with subscriber:
     try:
-        create_stop_event_table(cur)
+        streaming_pull_future.result()
+    except KeyboardInterrupt:
+        streaming_pull_future.cancel()
+        streaming_pull_future.result()
 
-        # Run the subscriber continuously
-        while True:
-            subscriber = pubsub_v1.SubscriberClient()
-            subscription_path = subscriber.subscription_path(project_id, subscription_id)
-            with subscriber:
-                streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-                try:
-                    streaming_pull_future.result(timeout=180)
-                except TimeoutError:
-                    print(f"Dumping csv records to database.")
-                    # Copy the CSV data to the database
-                    copy_csv_to_db(tempfile_path, 'stopevent', cur)
-                    print(f"Completed Dumping csv records to database.")
-
-                    #logic to update trip table
-                    print(f"Completed updating stopevent records to trip.")
-
-                    os.remove(tempfile_path)
-
-                except Exception as e:
-                    print(f"Error: {e}")
-                    streaming_pull_future.cancel()
-                    streaming_pull_future.result()
-
-    finally:
-        cur.close()
-        conn.close()
